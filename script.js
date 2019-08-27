63d27647dcbca7913a03e6a5720c4b7c3fe0e71d
Batimusprime <primebatimus@gmail.com>
2019-08-27 01:26:01 -0700

    Almost all functionality working

--- script.js
+++ script.js
@@ -39,32 +39,20 @@ $(document).ready(function(){
     */
 
    M.AutoInit();
+ 
 
-   $('.sidenav').sidenav();
-   let elem = document.getElementById('slide-out');
-   let instance = M.Sidenav.getInstance(elem);
-   
-   
-   //auto open menu
-   // instance.open();
-   $('#burger').click(function(){
-   
-    instance.open();
-   
-   
-   })
-   
-   $('#close').click(function(){
-   
-    instance.close();
-   
-   })
+   $('.fixed-action-btn').floatingActionButton({
 
+        toolbarEnabled: true
+    
+    });
+  
     /* 
     
         ////WRITE TASKS TO PAGE////
 
     */
+
     function populate(){
 
         //clear div first
@@ -72,18 +60,17 @@ $(document).ready(function(){
          
         //session counter so that tasks stay in order while displayed
         counter = 1;
-        //child added is for getting lists, it is recursive, value is not
+
+        //get all data from fb one time
         ref.once("value").then(function(data) { 
                 
-            $('#emoji').html('SAD');
-
+            dispTaskNum = data.numChildren();            
 
             //built in snapshot function
             data.forEach((child) =>{
 
                 //generate a random number                
-                imgCounter = Math.floor((Math.random()*4));
-                console.log('Random: ' + imgCounter)
+                imgCounter = getRandom(4);
 
                 //write the HTML
                 $('#target').append('<div class="card horizontal cardtainer" id="cardtainer'+ child.key +'"></div>')
@@ -92,10 +79,13 @@ $(document).ready(function(){
  
                 $('#cardtainer' + child.key).append('<div class="card-stacked"><div class="card-content" id="content' + child.key + '"><span>'+ counter +'.0</span> : <span>'+ child.val().name+'</span><hr><p>' + child.val().desc + '</p></div>');
  
-                $('#content' + child.key).append('<div class="card-action act"><i class="material-icons small up" id="complete' + child.key +'">done</i><i class="material-icons small down" id="delete' + child.key +'">clear</i><i></i></div></div></div>');      
+                $('#content' + child.key).append('<div class="card-action act"><i class="material-icons small up" id="complete' + child.key +'">done</i><i class="material-icons small down" id="delete' + child.key +'">clear</i><i class="material-icons small star" id="star' + child.key +'">star_border</i></div></div></div>');      
         
                 //iterate the session counter
-                counter++;   
+                counter++;
+                
+                //display a good status in the popout menu status section
+
                 $('#emoji').html('&#x1F44D;');
 
             })
@@ -110,32 +100,77 @@ $(document).ready(function(){
 
    */
 
-   function writeDB(){
-
-    ref.push().set({
-      name: 'Task Name Here' + iter,
-      desc: 'I need some unique content' + iter
-    });
+   function writeDB(tn,td){
     
-    writtenKey = ref.push().key;
-    console.log('Key ' + writtenKey + ' sesison written - ' + iter)
-    iter++;
-    populate();
-    
-   }
+        //push taskName and task Description to the db
+        ref.push().set({
+            
+            name: tn,
+            desc: td,
+            star: 'n'
+        
+        });
 
+        //repopulate the page
+        populate();
+        
+        //display status
+        M.toast({html: 'Task Written', classes: 'customToast', displayLength: '1500'})
+    
+    }
+
+   /*
+
+       ////FAVORITE A CARD////
+   */
+
+    function fav(x){
+        //check if icon is open
+        if( $('#star' + x).html() == 'star_border'){
+
+            //replace icon with filled in
+            $('#star' + x).html('star');
+
+            //update the db entry to indicate starred
+            ref.child(x).update({
+
+                star: 'y'                   
+            
+            });
+        
+            //notify user
+            M.toast({html: 'Favorite Added', classes: 'customToast', displayLength: '1500'})
+        
+        //check if icon is closed...
+        }else if($('#star' + x).html() == 'star'){
+
+            $('#star' + x).html('star_border');
+            ref.child(x).update({
+
+                star: 'n'
+            
+            });
+            
+            M.toast({html: 'Favorite Removed', classes: 'customToast', displayLength: '1500'})
+
+        }
+
+    }
+   
    /*
 
        ////LISTENERS////
    
    */
 
+   //add task on Floating action button toolbar FABT
     $('#addTask').on('click', function(){
         
         writeDB();
     
     })
 
+    //delete button
     $('#target').on('click', '[id^=delete]', function() {   
 
         //get the unique id (used by FB) of the card to delete
@@ -143,25 +178,110 @@ $(document).ready(function(){
         
         //remove entry in DB
         firebase.database().ref('batman/' + toDel).remove();
-    
+        M.toast({html: 'Task Removed', classes: 'customToast', displayLength: '1500'})
+
         //repopulate the page
         populate();
     });
 
-    $('#floatPlus').click(function(){
 
-        writeDB();
+    //change icon pics on click
+    //set listener on static parent with selector as second arg
+    $('#target').on('click', '.icon', function() { 
+ 
+        //get a new random number. arg is number of available images
+        newRand = getRandom(8)
+    
+        //use the id of the clicked element as a selector, change the src attribute to a new path with a random file identifier
+        $('#' + this.id).attr('src', 'icon' + newRand + '.png')
+        
+    });
+
+
+    //click on star to toggle between filled and empty outline, change status to 'y' in db. Will eventually pin to top / manually select as 'random task'
+    $('#target').on('click','.star', function(){
+
+        fav(this.id.slice(4));
+        console.log(this.id.slice(4))
 
     })
 
+    //Click on check will set card as completed
+    $('#target').on('click','.up', function(){
+
+        //add CSS class to clicked card container
+        $('#cardtainer' + this.id.slice(8)).addClass('completed');
+        M.toast({html: 'Completed', classes: 'customToast', displayLength: '1500'})
+
+    })
+    
+    //Submit a task
+    $('#subTask').click(function(){
+
+        taskName = $('#name').val();
+        taskDesc = $('#desc').val();
+        if(!taskName || !taskDesc || taskName == ' ' || taskDesc == ' ' || taskDesc == undefined || taskName == undefined){
+        
+            M.toast({html: 'Error: No Data Submitted', classes: 'customToast', displayLength: '1500'})
 
 
+        }else{
+            //test write with params
+            writeDB('Default Task Name', 'Task Descriptions Can Get Very Long So They Should Hopefully Wrap')
+            
+            //REMOVE FOR PRODUCTION
+            // writeDB(taskName, taskDesc);
+            console.log('submitted!')
+            console.log(taskName + " : " + taskDesc);
+        
+        }
+    })
+
+
+    $('#randTask').click(function(){
+
+        //number of tasks displayed
+        console.log(dispTaskNum);
+    
+        //check if no tasks are displayed
+        if (dispTaskNum == 0){
+    
+            M.toast({html: 'Error: No Posts Displayed', classes: 'customToast', displayLength: '1500'})
+
+        }
+        //get a random number with number of possible tasks as ceiling
+        newRand = getRandom(dispTaskNum)
+        console.log(newRand + ' should be less than ^^ ');
+
+        let q = document.getElementById('target').getElementsByClassName('cardtainer')[newRand];
+
+        //call only first part of favorite on randomly selected card
+        $('#star' + q.id.slice(10)).html('star');
+        ref.child(q.id.slice(10)).update({
+
+            star: 'y'
+
+        });
+
+
+
+    })
     /*
         ////RANDOM FUNCS////
         
     */
 
+    //return a random number with arg as limit
+    function getRandom(x){
+
+
+        return Math.floor((Math.random()*x));
+
+    }
+
     //initialize the list upon reload
     populate();
+    
 
-});//end doc ready
+
+ });//end doc ready
